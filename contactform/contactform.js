f263da4fc99af9f75901189c22718382fdabbec5
Aryan <aryansahal19@gmail.com>
2022-12-17 22:49:10 +0530

    first commit

---
+++ contactform/contactform.js
@@ -0,0 +1,56 @@
+// Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "AIzaSyBMgKoISM8MJdr88ARHq_KQj9w5lXwoaWk",
+  authDomain: "cel-bits-goa-contact-us-form.firebaseapp.com",
+  databaseURL: "https://cel-bits-goa-contact-us-form.firebaseio.com",
+  projectId: "cel-bits-goa-contact-us-form",
+  storageBucket: "cel-bits-goa-contact-us-form.appspot.com",
+  messagingSenderId: "550138663766",
+  appId: "1:550138663766:web:04d08e8356bc0ad4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+  var messageRef = firebase.database().ref('message');          
+  
+  document.getElementById('contact_form').addEventListener('submit',submitform); 
+  
+  function getIdValue(id) 
+  {                                             
+      return document.getElementById(id).value;     
+  }                                                         
+  
+  function submitform(e)    
+  {                                                 
+      e.preventDefault();                           
+  
+      var name = getIdValue('name');                    
+      var email = getIdValue('email');                            
+      var message = getIdValue('message');                  
+  
+      console.log(name);        
+      
+      saveMessage(name,email,message);
+  
+      name = "";
+      email = "";
+      message = "";
+  
+      document.querySelector('.alert').style.display = 'block';
+  
+      setTimeout(function(){
+          document.querySelector('.alert').style.display = 'none';
+      },3000);
+
+      document.getElementById('contact_form').reset();
+  }
+  
+  function saveMessage(name,email,message)                              
+  {
+      var newMessageRef = messageRef.push();
+      newMessageRef.set({
+          name: name,
+          email: email,
+          message: message
+      })
+  }
---
+++ js/main.js
@@ -0,0 +1,191 @@
+/*global $, jQuery, alert*/
+$(document).ready(function() {
+
+  'use strict';
+
+  // ========================================================================= //
+  //  //SMOOTH SCROLL
+  // ========================================================================= //
+
+
+  $(document).on("scroll", onScroll);
+
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('a').each(function() {
+      $(this).removeClass('active');
+      if ($(window).width() < 768) {
+        $('.nav-menu').slideUp();
+      }
+    });
+
+    $(this).addClass('active');
+
+    var target = this.hash,
+        menu = target;
+
+    target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': target.offset().top - 80
+    }, 500, 'swing', function() {
+      window.location.hash = target.selector;
+      $(document).on("scroll", onScroll);
+    });
+  });
+
+
+  function onScroll(event) {
+    if ($('.home').length) {
+      var scrollPos = $(document).scrollTop();
+      $('nav ul li a').each(function() {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+      });
+    }
+  }
+
+  // ========================================================================= //
+  //  //NAVBAR SHOW - HIDE
+  // ========================================================================= //
+
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 200 ) {
+      $("#main-nav, #main-nav-subpage").slideDown(700);
+      $("#main-nav-subpage").removeClass('subpage-nav');
+    } else {
+      $("#main-nav").slideUp(700);
+      $("#main-nav-subpage").hide();
+      $("#main-nav-subpage").addClass('subpage-nav');
+    }
+  });
+
+  // ========================================================================= //
+  //  // RESPONSIVE MENU
+  // ========================================================================= //
+
+  $('.responsive').on('click', function(e) {
+    $('.nav-menu').slideToggle();
+  });
+
+  // ========================================================================= //
+  //  Typed Js
+  // ========================================================================= //
+
+  var typed = $(".typed");
+
+  $(function() {
+    typed.typed({
+      strings: ["Inspire.", "Innovate.", "Ideate."],
+      typeSpeed: 100,
+      loop: true,
+    });
+  });
+
+
+  // ========================================================================= //
+  //  Owl Carousel Services
+  // ========================================================================= //
+
+
+  $('.services-carousel').owlCarousel({
+      autoplay: true,
+      loop: true,
+      margin: 20,
+      dots: true,
+      nav: false,
+      responsiveClass: true,
+      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
+    });
+
+  // ========================================================================= //
+  //  magnificPopup
+  // ========================================================================= //
+
+  var magnifPopup = function() {
+    $('.popup-img').magnificPopup({
+      type: 'image',
+      removalDelay: 300,
+      mainClass: 'mfp-with-zoom',
+      gallery: {
+        enabled: true
+      },
+      zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+          // openerElement is the element on which popup was initialized, in this case its <a> tag
+          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+  };
+
+
+  // Call the functions
+  magnifPopup();
+
+});
+
+// ========================================================================= //
+//  Porfolio isotope and filter
+// ========================================================================= //
+$(window).load(function(){
+
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-thumbnail',
+    layoutMode: 'fitRows'
+  });
+
+  $('#portfolio-flters li').on( 'click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+  });
+
+})
+
+//slideup - fadein
+function isElementInViewport(elem) {
+  var $elem = $(elem);
+
+  // Get the scroll position of the page.
+  var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+  var viewportTop = $(scrollElem).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  // Get the position of the element on the page.
+  var elemTop = Math.round( $elem.offset().top );
+  var elemBottom = elemTop + $elem.height();
+
+  return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+  var $elem = $('.abt-heading2');
+
+  // If the animation has already been started
+  if ($elem.hasClass('animate__zoomIn')) return;
+
+  if (isElementInViewport($elem)) {
+      // Start the animation
+      $elem.addClass('animate__zoomIn');
+  }
+}
+
+// Capture scroll events
+$(window).scroll(function(){
+  checkAnimation();
+}); 
